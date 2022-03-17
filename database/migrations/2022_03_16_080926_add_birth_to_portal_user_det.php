c11e3681c49e27bf8e1884adea0bed0e3f836795
denyrachmat22 <denyrachmat22@gmail.com>
2022-03-17 17:08:36 +0700

    Feat: Forget to add resident column

--- database/migrations/2022_03_16_080926_add_birth_to_portal_user_det.php
+++ database/migrations/2022_03_16_080926_add_birth_to_portal_user_det.php
@@ -13,11 +13,17 @@ class AddBirthToPortalUserDet extends Migration
      */
     public function up()
     {
-        Schema::table('portal_user_det', function (Blueprint $table) {
+        Schema::table('portal_users_det', function (Blueprint $table) {
+            $table->string('pud_id_type')->nullable();
             $table->string('pud_birth_place')->nullable();
             $table->date('pud_birth_date')->nullable();
+            $table->string('pud_country_rsdn')->nullable();
+            $table->string('pud_district_rsdn')->nullable();
+            $table->string('pud_subdistrict_rsdn')->nullable();
+            $table->text('pud_addr1_rsdn')->nullable();
+            $table->text('pud_addr2_rsdn')->nullable();
         });
-    }
+    }   
 
     /**
      * Reverse the migrations.
