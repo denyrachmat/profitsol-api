3879eeb017d7ad50d28a842a816dbdb80eec5460
denyrachmat22 <denyrachmat22@gmail.com>
2022-03-22 18:28:56 +0700

    Feat: Add country list

---
+++ app/Http/Controllers/API/PORTAL/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API\PORTAL;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\PORTAL\BaseController as BaseController;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileController extends BaseController
+{
+    public function getCountryList()
+    {
+        $data = Storage::disk('public')->get('countries+states+cities.json');
+
+        return $data;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PORTAL\AuthController;
+use App\Http\Controllers\API\PORTAL\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
+Route::get('countryList', [ProfileController::class, 'getCountryList']);
