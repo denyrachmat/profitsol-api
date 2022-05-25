04c5957ac11dc65e70a8f7e5c4ef595cf2afb553
denyrachmat22 <denyrachmat22@gmail.com>
2022-05-25 17:39:17 +0700

    Feat: Continue Profile

--- app/Http/Controllers/API/PORTAL/ProfileController.php
+++ app/Http/Controllers/API/PORTAL/ProfileController.php
@@ -20,6 +20,25 @@ class ProfileController extends BaseController
     }
 
     public function store(UserDetRequest $request) {
+        $dataReqConvertToDB = [
+            'u_username' => $request->header('Username'),
+            'pud_id_card' => $request->IDNum,
+            'pud_photo' => $request->ava,
+            'pud_country' => '',
+            'pud_states' => '',
+            'pud_district' => '',
+            'pud_subdistrict' => '',
+            'pud_addr1' => '',
+            'pud_addr2' => '',
+            'pud_id_type' => '',
+            'pud_birth_place' => '',
+            'pud_birth_date' => '',
+            'pud_country_rsdn' => '',
+            'pud_district_rsdn' => '',
+            'pud_subdistrict_rsdn' => '',
+            'pud_addr1_rsdn' => '',
+            'pud_addr2_rsdn' => ''
+        ];
         return $request;
         return PortalUserDet::create($request);
     }
--- app/Http/Requests/PORTAL/UserDetRequest.php
+++ app/Http/Requests/PORTAL/UserDetRequest.php
@@ -24,7 +24,6 @@ class UserDetRequest extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required',
             'firstName' => 'required',
             'lastName' => 'required'
         ];
