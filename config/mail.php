25ef704bb955fe456bd5dca86164e4f10fc4bf08
denyrachmat22 <denyrachmat22@gmail.com>
2022-06-20 17:50:08 +0700

    Feat: Add Mail and saml

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,14 @@ return [
             'username' => env('MAIL_USERNAME', 'stxi-server@sumitronics.co.jp'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => null
+            'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
@@ -59,7 +66,7 @@ return [
 
         'sendmail' => [
             'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -t -i'),
+            'path' => env('MAIL_SENDMAIL_PATH', 'C:/laragon/bin/sendmail/sendmail.exe'),
         ],
 
         'log' => [
