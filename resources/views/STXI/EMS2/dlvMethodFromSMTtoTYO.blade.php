4422a7a2e112794e16414e653f73f45661e69b2a
Deny Rachmat <denyrachmat22@gmail.com>
2022-08-01 12:08:25 +0700

    Fix: 0 return SPQ

--- resources/views/STXI/EMS2/dlvMethodFromSMTtoTYO.blade.php
+++ resources/views/STXI/EMS2/dlvMethodFromSMTtoTYO.blade.php
@@ -33,7 +33,7 @@
                     <td style="background-color:#fff;border-color:inherit;border-style:solid;border-width:1px;color:#333;font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal">{{$value['TOT_INC_DLV']}}</td>
                     <td style="background-color:#fff;border-color:inherit;border-style:solid;border-width:1px;color:#333;font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal">{{$value['TOT_OUT_BC_DLV']}}</td>
                     <td style="background-color:#fff;border-color:inherit;border-style:solid;border-width:1px;color:#333;font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal">
-                        @if(count($value['SPQ']) > 0)
+                        @if($value['SPQ'] !== '0' || count($value['SPQ']) > 0)
                         @foreach($value['SPQ'] as $keySPQ => $valSPQ)
                             {{$valSPQ}} <br>
                         @endforeach
