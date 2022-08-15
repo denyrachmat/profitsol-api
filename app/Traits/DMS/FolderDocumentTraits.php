a1c965e15314268d8d64aaa31eb2ae9acf8b2f17
denyrachmat22 <denyrachmat22@gmail.com>
2022-08-16 02:11:57 +0700

    Fix: Test 4

--- app/Traits/DMS/FolderDocumentTraits.php
+++ app/Traits/DMS/FolderDocumentTraits.php
@@ -159,7 +159,8 @@ trait FolderDocumentTraits
             $expFolder = explode('/', $value['folders_name']);
             $dataDBFolder = DMSFolderMstr::where('dfm_folder_name', $expFolder[count($expFolder) - 1])->first();
             $cekParent = DMSFolderMstr::where('dfm_folder_name', $expFolder[count($expFolder) - 2])->first();
-            $checkParent = count($expFolder) > 1
+
+            $checkParent = !empty($cekParent) && count($expFolder) > 1
                 ? $cekParent->id
                 : NULL;
 
