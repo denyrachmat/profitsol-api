416305a2c67306405896f32b8a4c6a1a688525e4
denyrachmat22 <denyrachmat22@gmail.com>
2022-08-15 23:03:38 +0700

    Fix: Path 2

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,7 +52,7 @@ return [
 
         'data_iso_2' => [
             'driver' => 'local',
-            'root'   => 'C:/Users/administrator.STX-I/AppData/Roaming/Microsoft/Windows',
+            'root'   => 'C:/Users/administrator.STX-I/AppData/Roaming/Microsoft/Windows/Network Shortcuts',
         ],
 
         'public' => [
