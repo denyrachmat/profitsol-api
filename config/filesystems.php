0c926f66b04dce9b71ff6fc44168f8ed75e465b2
denyrachmat22 <denyrachmat22@gmail.com>
2022-08-16 00:11:41 +0700

    Fix: Test 6

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,7 @@ return [
 
         'data_iso' => [
             'driver' => 'local',
-            'root'   => '\\\\192.168.100.7',
+            'root'   => '\\\\192.168.100.7\Document ISO 9001 2015',
         ],
 
         'data_iso_2' => [
