fbfa2f88c72d96e1b2c07c176fde893db856fe6e
denyrachmat22 <denyrachmat22@gmail.com>
2022-08-16 02:20:08 +0700

    Fix: Test 7

--- app/Traits/DMS/FolderDocumentTraits.php
+++ app/Traits/DMS/FolderDocumentTraits.php
@@ -132,7 +132,7 @@ trait FolderDocumentTraits
         if ($parentKey === 0) {
             return [
                 'key' => 0,
-                'folders_name' => $this->getAliasFolderbyAuthor($author),
+                'folders_name' => $this->getAliasFolderbyAuthor($author).'/'.$path,
                 'list_files' => Storage::disk($this->getAliasFolderbyAuthor($author, 'root'))->files($path),
                 'children' => $hasil
             ];
@@ -153,7 +153,7 @@ trait FolderDocumentTraits
                 ? $this->convertFolderPathToArray($author, $path)
                 : [$this->convertFolderPathToArray($author, $path)];
 
-            // return $data;
+            return $this->convertFolderPathToArray($author, $path);
         }
 
         $hasil = [];
