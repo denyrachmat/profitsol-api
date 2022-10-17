b384babb1ba5194c301a783664e2a450cac55246
Deny Rachmat <denyrachmat22@gmail.com>
2022-10-17 10:06:43 +0700

    Fix: to contain if

--- app/Imports/STXI/importRawPO.php
+++ app/Imports/STXI/importRawPO.php
@@ -26,11 +26,11 @@ class importRawPO implements ToModel, WithStartRow
             if ($key > 12 && $key < 75 && $key % 2 === 0) {
                 $checkDate = date('Y-m', strtotime($this->date)).'-'.$countDate;
 
-                if (date('w', strtotime($checkDate)) === 0) {
+                if (date('w', strtotime($checkDate)) == 0) {
                     $countDate = $countDate + 2;
                 }
 
-                if (date('w', strtotime($checkDate)) === 6) {
+                if (date('w', strtotime($checkDate)) == 6) {
                     $countDate = $countDate + 1;
                 }
 
