9725db76968742da4a69d6ebfceea8cc4e7e7bf3
Deny Rachmat <denyrachmat22@gmail.com>
2022-10-17 09:22:30 +0700

    Fix: Add checkdate

--- app/Imports/STXI/importRawPO.php
+++ app/Imports/STXI/importRawPO.php
@@ -24,15 +24,17 @@ class importRawPO implements ToModel, WithStartRow
         $countDate = 1;
         foreach ($row as $key => $value) {
             if ($key > 12 && $key < 75 && $key % 2 === 0) {
-                $date = date('Y-m', strtotime($this->date)).'-'.$countDate;
+                $checkDate = date('Y-m', strtotime($this->date)).'-'.$countDate;
 
-                if (date('D', strtotime($date)) === 'Sat') {
+                if (date('D', strtotime($checkDate)) === 'Sat') {
                     $countDate = $countDate + 2;
                 }
 
-                if (date('D', strtotime($date)) === 'Sun') {
+                if (date('D', strtotime($checkDate)) === 'Sun') {
                     $countDate = $countDate + 1;
                 }
+
+                $date = date('Y-m', strtotime($this->date)).'-'.$countDate;
 
                 if (!empty($value)) {
                     $item = $row[0];
