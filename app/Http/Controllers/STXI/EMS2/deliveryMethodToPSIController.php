e9d4ea259eb8770e366bead28c1f8a93320484bc
Deny Rachmat <denyrachmat22@gmail.com>
2022-10-17 17:27:00 +0700

    Fix: Change position delete

--- app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
+++ app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
@@ -537,6 +537,7 @@ class deliveryMethodToPSIController extends BaseController
                 }
                 foreach ($getID as $keyID => $valueID) {
                     if ($isSave) {
+                        DLVTYODet::where('DRST_ID', $valueID['id'])->forceDelete();
                         if ($valueID['IO_REMARK'] == 'TO_ITEC') {
                             $valFifo = "'" . $value['MITM_MODELCD'] . "', " . $value['TOT_OUT_BC_DLV'] . ", '" . date('Y-m-01', empty($dateFifoStart) ? strtotime('-1 month', strtotime($date)) : strtotime($dateFifoStart)) . "', '" . date('Y-m-01', strtotime($date)) . "'";
                             $hasil[$value['MITM_MODELCD']]['DATA_DATE'][$value['DEL_DATE']][$keyID]['DLVQT'] = $value['TOT_OUT_BC_DLV'];
@@ -551,8 +552,6 @@ class deliveryMethodToPSIController extends BaseController
                             ->table("Z_STXI_FIFO_OS_SO(" . $valFifo . ")")
                             ->get()
                             ->toArray();
-
-                        DLVTYODet::where('DRST_ID', $valueID['id'])->forceDelete();
 
                         $statInsert = [];
                         foreach ($dataFIfo as $keyInsert => $valueInsert) {
