52fb9336d9a45df202d5c090b4b9a934d4eeb56a
denyrachmat22 <denyrachmat22@gmail.com>
2022-11-15 14:38:07 +0700

    Fix: Remove bal cpo > 0

--- app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
+++ app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
@@ -498,7 +498,9 @@ class deliveryMethodToPSIController extends BaseController
             $query
         )));
 
-        $getCPO = $dataCPO->where('BAL_STOCK', '>', 0)->where('BAL_CPO_STXI_ITEC', '>', 0)->map(function ($t) {
+        $getCPO = $dataCPO->where('BAL_STOCK', '>', 0)
+            // ->where('BAL_CPO_STXI_ITEC', '>', 0)
+            ->map(function ($t) {
             return collect($t)->only([
                 'MITM_ITMCD',
                 'MITM_ITMD1',
