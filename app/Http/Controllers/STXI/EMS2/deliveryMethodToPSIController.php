0cd71a5ecf062627044ff38b2a1df1ff208cc351
denyrachmat22 <denyrachmat22@gmail.com>
2022-11-30 16:37:48 +0700

    Fix: Change to call

--- app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
+++ app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
@@ -259,7 +259,7 @@ class deliveryMethodToPSIController extends BaseController
             $date_to = date('d', strtotime($req->date)) == 1 ? date('Y-m-d') : date('Y-m-d', strtotime($req->date . "-1 days"));
             $query = "SET NOCOUNT ON; EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @model = '" . $value . "', @date_start = '" . date('Y-m-01', strtotime($req->date)) . "', @date_to = '" . $date_to . "'";
 
-            $dataCPO = DB::connection('sqlsrv_mega_tyo')->update("SET NOCOUNT ON; EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC ?, ?, ?", [$value, date('Y-m-01', strtotime($req->date)), $date_to]);
+            $dataCPO = DB::connection('sqlsrv_mega_tyo')->select("CALL Z_STXI_GET_CPO_DLV_STXI_ITEC ?, ?, ?", [$value, date('Y-m-01', strtotime($req->date)), $date_to]);
 
             return $dataCPO;
             $getSPQDataPersheet = $this->SPQIndex($value)->original['data'] ? $this->SPQIndex($value)->original['data']['MITM_SPQ_CHECK'] : false;
