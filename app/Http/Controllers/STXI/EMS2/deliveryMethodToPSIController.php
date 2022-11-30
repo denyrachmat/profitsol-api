930c77fe969c3469acaf9c88146016343f8484be
denyrachmat22 <denyrachmat22@gmail.com>
2022-11-30 15:21:07 +0700

    Fix: Add nocount

--- app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
+++ app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
@@ -257,7 +257,7 @@ class deliveryMethodToPSIController extends BaseController
         $hasil = [];
         foreach ($req->model as $key => $value) {
             $date_to = date('d', strtotime($req->date)) == 1 ? date('Y-m-d') : date('Y-m-d', strtotime($req->date . "-1 days"));
-            $query = "SET NOCOUNT ON;EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @model = '" . $value . "', @date_start = '" . date('Y-m-01', strtotime($req->date)) . "', @date_to = '" . $date_to . "'";
+            $query = "SET NOCOUNT ON; EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @model = '" . $value . "', @date_start = '" . date('Y-m-01', strtotime($req->date)) . "', @date_to = '" . $date_to . "'";
 
             $dataCPO = collect(DB::connection('sqlsrv_mega_tyo')->select(DB::raw(
                 $query
@@ -498,9 +498,9 @@ class deliveryMethodToPSIController extends BaseController
 
         $date_to = (int)date('d', strtotime($date)) == 1 ? date('Y-m-d', strtotime($date . "-1 days")) : date('Y-m-d');
         if (!empty($item)) {
-            $query = "SET NOCOUNT ON;EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @date_start = '" . date('Y-m-01', strtotime($date)) . "', @date_to = '" . $date . "', @model = '" . $item . "'";
+            $query = "SET NOCOUNT ON; EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @date_start = '" . date('Y-m-01', strtotime($date)) . "', @date_to = '" . $date . "', @model = '" . $item . "'";
         } else {
-            $query = "SET NOCOUNT ON;EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @date_start = '" . date('Y-m-01', strtotime($date)) . "', @date_to = '" . $date . "'";
+            $query = "SET NOCOUNT ON; EXEC Z_STXI_GET_CPO_DLV_STXI_ITEC @date_start = '" . date('Y-m-01', strtotime($date)) . "', @date_to = '" . $date . "'";
         }
 
         // return $query;
