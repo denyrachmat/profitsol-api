cea30d54d77991c2cedf83b1632fb38de3c8626e
denyrachmat22 <denyrachmat22@gmail.com>
2022-12-08 18:39:03 +0700

    Fix: Add Exec time

--- app/Http/Controllers/STXI/LOG/INSWDataController.php
+++ app/Http/Controllers/STXI/LOG/INSWDataController.php
@@ -49,7 +49,7 @@ class INSWDataController extends BaseController
     public function getListHSCode($hsCode = '', $maxSize = 200)
     {
         ini_set('memory_limit', '2G');
-        ini_set('max_execution_time', '300');
+        ini_set('max_execution_time', '10800');
         $data = $this->getListMaster($hsCode === 0 || !empty($hsCode) ? '' : $hsCode, $maxSize)['data'][0]['result'];
 
         $hasilData = [];
