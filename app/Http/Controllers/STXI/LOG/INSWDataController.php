af64e98928a7a874f4b6973b0c60bf6f8087c5c9
denyrachmat22 <denyrachmat22@gmail.com>
2022-12-08 17:53:01 +0700

    Fix: Add Error catch

--- app/Http/Controllers/STXI/LOG/INSWDataController.php
+++ app/Http/Controllers/STXI/LOG/INSWDataController.php
@@ -54,98 +54,110 @@ class INSWDataController extends BaseController
         foreach ($data as $key => $value) {
             $getHSCode = $value['_source']['hs_code_format'];
 
-            $dataDetail = $this->getData($getHSCode);
+            try {
+                $dataDetail = $this->getData($getHSCode);
 
-            if (!empty($dataDetail)) {
-                if (count($dataDetail) > 0) {
-                    $dataDetailGet = $dataDetail[0];
-                    $dataHSParent = $dataDetailGet['hsParent'][0];
-                    $dataHSParentFrmt = $dataDetailGet['hsParent'][1];
-                    $dataMFN = $dataDetailGet['mfn'][0];
+                if (!empty($dataDetail)) {
+                    if (count($dataDetail) > 0) {
+                        $dataDetailGet = $dataDetail[0];
+                        $dataHSParent = $dataDetailGet['hsParent'][0];
+                        $dataHSParentFrmt = $dataDetailGet['hsParent'][1];
+                        $dataMFN = $dataDetailGet['mfn'][0];
 
-                    $masterCreate = INSWDataMaster::updateOrCreate([
-                        'ZID_HSCODE' => $getHSCode,
-                    ], [
-                        'ZID_HSCODE' => $getHSCode,
-                        'ZID_BAGIAN' => $dataDetailGet['bagian'],
-                        'ZID_BAB' => $dataDetailGet['bab'],
-                        'ZID_HSPRNT' => $dataHSParent['hs_code_format'],
-                        'ZID_HSPRNT_DESC_ID' => $dataHSParent['ur_id'],
-                        'ZID_HSPRNT_DESC_EN' => $dataHSParent['ur_en'],
-                        'ZID_HSPRNT_FRMT' => $dataHSParentFrmt['hs_code_format'],
-                        'ZID_HSPRNT_FRMT_DESC_ID' => $dataHSParentFrmt['ur_id'],
-                        'ZID_HSPRNT_FRMT_DESC_END' => $dataHSParentFrmt['ur_en'],
-                        'ZID_MFN_BM' => $dataMFN['bm'][0]['bm'],
-                        'ZID_MFN_PPN' => $dataMFN['ppn'][0]['ppn'],
-                        'ZID_MFN_PPH' => $dataMFN['pph'][0]['pph'],
-                        'ZID_KOND' => $dataDetailGet['kondisiTertentu'],
-                    ]);
-
-                    $jlsCreate = [];
-                    foreach ($dataDetailGet['bagian_penjelasan'] as $keyJls => $valueJls) {
-                        $jlsCreate[] = INSWDataJlsDetail::updateOrCreate([
+                        $masterCreate = INSWDataMaster::updateOrCreate([
                             'ZID_HSCODE' => $getHSCode,
                         ], [
                             'ZID_HSCODE' => $getHSCode,
-                            'ZIJD_DET_ID' => $valueJls,
-                            'ZIJD_DET_EN',
+                            'ZID_BAGIAN' => $dataDetailGet['bagian'],
+                            'ZID_BAB' => $dataDetailGet['bab'],
+                            'ZID_HSPRNT' => $dataHSParent['hs_code_format'],
+                            'ZID_HSPRNT_DESC_ID' => $dataHSParent['ur_id'],
+                            'ZID_HSPRNT_DESC_EN' => $dataHSParent['ur_en'],
+                            'ZID_HSPRNT_FRMT' => $dataHSParentFrmt['hs_code_format'],
+                            'ZID_HSPRNT_FRMT_DESC_ID' => $dataHSParentFrmt['ur_id'],
+                            'ZID_HSPRNT_FRMT_DESC_END' => $dataHSParentFrmt['ur_en'],
+                            'ZID_MFN_BM' => $dataMFN['bm'][0]['bm'],
+                            'ZID_MFN_PPN' => $dataMFN['ppn'][0]['ppn'],
+                            'ZID_MFN_PPH' => $dataMFN['pph'][0]['pph'],
+                            'ZID_KOND' => $dataDetailGet['kondisiTertentu'],
                         ]);
-                    }
 
-                    $jlsCreateEn = [];
-                    foreach ($jlsCreate as $keyJls2 => $valueJls2) {
-                        $jlsCreateEn[] = INSWDataJlsDetail::where('id', $valueJls2['id'])->update([
-                            'ZIJD_DET_EN' => $valueJls2
-                        ]);
-                    }
+                        $jlsCreate = [];
+                        foreach ($dataDetailGet['bagian_penjelasan'] as $keyJls => $valueJls) {
+                            $jlsCreate[] = INSWDataJlsDetail::updateOrCreate([
+                                'ZID_HSCODE' => $getHSCode,
+                            ], [
+                                'ZID_HSCODE' => $getHSCode,
+                                'ZIJD_DET_ID' => $valueJls,
+                                'ZIJD_DET_EN',
+                            ]);
+                        }
 
-                    $satImp = [];
-                    foreach ($dataDetailGet['refSatuan']['impor'] as $keySatImp => $valueSatimp) {
-                        $satImp[] = INSWDataSatDetail::updateOrCreate([
-                            'ZID_HSCODE' => $getHSCode,
-                        ], [
-                            'ZISD_TYPE' => 'import',
-                            'ZISD_SERI' => $valueSatimp['seri'],
-                            'ZISD_JENIS' => $valueSatimp['kd_satuan'],
-                            'ZISD_SATUAN' => $valueSatimp['ur_satuan'],
-                        ]);
-                    }
+                        $jlsCreateEn = [];
+                        foreach ($jlsCreate as $keyJls2 => $valueJls2) {
+                            $jlsCreateEn[] = INSWDataJlsDetail::where('id', $valueJls2['id'])->update([
+                                'ZIJD_DET_EN' => $valueJls2
+                            ]);
+                        }
 
-                    $satExp = [];
-                    foreach ($dataDetailGet['refSatuan']['impor'] as $keySatImp => $valueSatimp) {
-                        $satExp[] = INSWDataSatDetail::updateOrCreate([
-                            'ZID_HSCODE' => $getHSCode,
-                        ], [
-                            'ZISD_TYPE' => 'export',
-                            'ZISD_SERI' => $valueSatimp['seri'],
-                            'ZISD_JENIS' => $valueSatimp['kd_satuan'],
-                            'ZISD_SATUAN' => $valueSatimp['ur_satuan'],
-                        ]);
-                    }
+                        $satImp = [];
+                        foreach ($dataDetailGet['refSatuan']['impor'] as $keySatImp => $valueSatimp) {
+                            $satImp[] = INSWDataSatDetail::updateOrCreate([
+                                'ZID_HSCODE' => $getHSCode,
+                            ], [
+                                'ZISD_TYPE' => 'import',
+                                'ZISD_SERI' => $valueSatimp['seri'],
+                                'ZISD_JENIS' => $valueSatimp['kd_satuan'],
+                                'ZISD_SATUAN' => $valueSatimp['ur_satuan'],
+                            ]);
+                        }
 
-                    $hasilData[] = [
-                        'status' => true,
-                        'message' => 'Data berhasil di update',
-                        'storedMaster' => $masterCreate,
-                        'storedPenjelasanDetID' => $jlsCreate,
-                        'storedPenjelasanDetEN' => $jlsCreateEn,
-                        'storedStatusDet' => $satExp
-                    ];
+                        $satExp = [];
+                        foreach ($dataDetailGet['refSatuan']['impor'] as $keySatImp => $valueSatimp) {
+                            $satExp[] = INSWDataSatDetail::updateOrCreate([
+                                'ZID_HSCODE' => $getHSCode,
+                            ], [
+                                'ZISD_TYPE' => 'export',
+                                'ZISD_SERI' => $valueSatimp['seri'],
+                                'ZISD_JENIS' => $valueSatimp['kd_satuan'],
+                                'ZISD_SATUAN' => $valueSatimp['ur_satuan'],
+                            ]);
+                        }
+
+                        $hasilData[] = [
+                            'status' => true,
+                            'message' => 'Data berhasil di update',
+                            'storedMaster' => $masterCreate,
+                            'storedPenjelasanDetID' => $jlsCreate,
+                            'storedPenjelasanDetEN' => $jlsCreateEn,
+                            'storedStatusDet' => $satExp
+                        ];
+                    } else {
+                        $hasilData[] = [
+                            'status' => false,
+                            'message' => 'Data detail sisa tidak ditemukan !!',
+                            'data' => $dataDetail,
+                            'storedMaster' => [],
+                            'storedPenjelasanDetID' => [],
+                            'storedPenjelasanDetEN' => [],
+                            'storedStatusDet' => []
+                        ];
+                    }
                 } else {
                     $hasilData[] = [
                         'status' => false,
-                        'message' => 'Data detail sisa tidak ditemukan !!',
-                        'data' => $dataDetail,
+                        'message' => 'Data detail tidak ditemukan !!',
                         'storedMaster' => [],
                         'storedPenjelasanDetID' => [],
                         'storedPenjelasanDetEN' => [],
                         'storedStatusDet' => []
                     ];
                 }
-            } else {
+            } catch (\Throwable $th) {
                 $hasilData[] = [
                     'status' => false,
-                    'message' => 'Data detail tidak ditemukan !!',
+                    'message' => 'Ada error di server !!',
+                    'data' => $th->getMessage(),
                     'storedMaster' => [],
                     'storedPenjelasanDetID' => [],
                     'storedPenjelasanDetEN' => [],
