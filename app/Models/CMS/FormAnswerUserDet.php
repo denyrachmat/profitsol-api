f48bc8166b89384294ce275313f87562ddb6ad8c
denyrachmat22 <denyrachmat22@gmail.com>
2023-01-04 19:38:42 +0700

    Feaet: Add quiz preview

--- app/Models/CMS/FormAnswerUserDet.php
+++ app/Models/CMS/FormAnswerUserDet.php
@@ -4,12 +4,13 @@ namespace App\Models\CMS;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class FormAnswerUserDet extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $connection = 'sqlsrv_cms';
     protected $table = 'cms_form_ans_user_det';
+    protected $dates = ['deleted_at'];
 
     protected $fillable = [
         'p_u_username',
