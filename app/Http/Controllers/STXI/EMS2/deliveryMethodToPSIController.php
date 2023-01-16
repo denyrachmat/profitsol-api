216ce9b7c0213c09dd298984c88601f79ef7c78f
denyrachmat22 <denyrachmat22@gmail.com>
2023-01-16 15:42:26 +0700

    Fix: Add table

--- app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
+++ app/Http/Controllers/STXI/EMS2/deliveryMethodToPSIController.php
@@ -1066,7 +1066,7 @@ class deliveryMethodToPSIController extends BaseController
     public function getPOTYOMegaReady($date)
     {
         $data = TYO_PO_MSTR::select(
-            'id',
+            'TYO_PO_MSTR.id',
             'TPM_ITMCD',
             'MITM_ITMD1',
             'TPM_ISSDT',
