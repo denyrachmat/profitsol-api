640f3c4a049affcd6abe2005a4d48cd303bf7388
denyrachmat22 <denyrachmat22@gmail.com>
2023-04-03 12:50:34 +0700

    Fix: Open all cors

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,4 @@
 <?php
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
-
 use App\Http\Controllers\API\CMS\FormController;
 use App\Http\Controllers\API\CMS\QuizController;
 use App\Http\Controllers\API\DMS\DocumentController;
