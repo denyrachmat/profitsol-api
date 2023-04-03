addcb6f96dcf6721b84cec54576a9899676a11a6
denyrachmat22 <denyrachmat22@gmail.com>
2023-04-03 11:54:04 +0700

    Fix: Coba lagi hadehhh

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://192.168.100.32:8081', '*'],
 
     'allowed_origins_patterns' => [],
 
