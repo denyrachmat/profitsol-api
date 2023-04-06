1b4d1ef711ec146fe2504be6ab6525a7b3ce06f7
denyrachmat22 <denyrachmat22@gmail.com>
2023-04-06 16:16:12 +0700

    Fix: Try to change loc

--- app/Http/Controllers/STXI/BIM/CircullarTenController.php
+++ app/Http/Controllers/STXI/BIM/CircullarTenController.php
@@ -383,16 +383,18 @@ class CircullarTenController extends BaseController
         curl_setopt($ch, CURLOPT_POST,1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $result=json_decode(curl_exec($ch));
-
         $status = '';
         if(curl_exec($ch) === false)
         {
             $status = 'Curl error: ' . curl_error($ch);
+            $result=json_decode(curl_exec($ch));
+    
         }
         else
         {
             $status = 'Operation completed without any errors, you have the response';
+            $result=json_decode(curl_exec($ch));
+    
         }
 
         $hasil = [
