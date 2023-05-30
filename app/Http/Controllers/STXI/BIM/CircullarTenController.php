bae7543f52f02fec1c32f2bf9f7b94f638c69298
denyrachmat22 <denyrachmat22@gmail.com>
2023-05-30 18:07:58 +0700

    show 1 by 1 getDataItem

--- app/Http/Controllers/STXI/BIM/CircullarTenController.php
+++ app/Http/Controllers/STXI/BIM/CircullarTenController.php
@@ -491,7 +491,7 @@ class CircullarTenController extends BaseController
                 $getDataItem = DB::connection('sqlsrv_mega_sme')->table('MITM_TBL')
                     ->where('MITM_ITMCD', 'like', $valueMdl->CIM_ITMCD . '%')
                     ->first();
-                    logger($getDataItem->all());
+                    logger([$getDataItem->MITM_ITMD1, $getDataItem->MITM_ITMTY, $getDataItem->MITM_SUPCD]);
                 if (!empty($getDataItem)) {
                     $hasil[(empty($getDataItem->MITM_SUPCD) ? substr($getDataItem->MITM_ITMTY, 0, 3) : substr($getDataItem->MITM_SUPCD, 0, 3))] = (empty($getDataItem->MITM_SUPCD) ? substr($getDataItem->MITM_ITMTY, 0, 3) : substr($getDataItem->MITM_SUPCD, 0, 3));
                     $hasilItem[] = [
