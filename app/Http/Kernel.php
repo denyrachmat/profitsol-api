b088b4a7ebec22cc4e674a03a7b66a5173333bfd
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-30 13:38:17 +0700

    Fix: Coba pake cors ori

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \App\Http\Middleware\CorsMiddleware::class,
+        // \App\Http\Middleware\CorsMiddleware::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         // \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
@@ -64,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \App\Http\Middleware\CorsMiddleware::class,
+        // 'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 }
