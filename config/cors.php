a2197ad04e2d4a282a90d6e3e7c812b2ce327fe0
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-30 15:27:55 +0700

    Fix: Add portal app FE cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://192.168.100.32:8081'],
 
     'allowed_origins_patterns' => ['*'],
 
