6a4e54ba0cbb44a07707d937ae51484e9ca89e38
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-30 16:13:07 +0700

    Fix: to *

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
 
         return $response;
     }
