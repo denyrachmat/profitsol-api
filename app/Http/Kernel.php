5e964a9af32c484cf45809a0fc516cc77f2e30fd
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-30 15:37:24 +0700

    Fix: Add cors mid

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\CorsMiddleware::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         // \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
@@ -23,6 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     /**
