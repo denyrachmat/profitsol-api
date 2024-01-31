f7975e0d11f7592a14734db3c705361b8cab75cc
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-31 17:00:03 +0700

    Fix: Failed sync mega server

--- app/Jobs/STXI/LOG/SyncITInventoryFromMega.php
+++ app/Jobs/STXI/LOG/SyncITInventoryFromMega.php
@@ -38,54 +38,63 @@ class SyncITInventoryFromMega implements ShouldQueue
      */
     public function handle()
     {
-        if ($this->isIfaceMega) {
-            Redis::publish('portalv2', json_encode([
-                'app' => 'it_inv_checker',
-                'message' => $this->date. ' start mega sync to it inventory now...',
-                'type' => 'info',
-                'status' => 'start_mega_resync',
-                'data' => $this->date 
-            ]));
-    
-            DB::connection('sqlsrv_itinv')->update("SET NOCOUNT ON;EXEC IF_CR_ALL_BYDAY @IFDT_Str='".$this->date."', @SUMFLG=1");
-    
-            Redis::publish('portalv2', json_encode([
-                'app' => 'it_inv_checker',
-                'message' => $this->date. ' data sync !! please check on IT Inventory',
-                'type' => 'green',
-                'status' => 'success_mega_resync',
-                'data' => $this->date 
-            ]));
-        } else {
-            Redis::publish('portalv2', json_encode([
-                'app' => 'it_inv_checker',
-                'message' => 'Mega sync skipping date : '.$this->date,
-                'type' => 'orange',
-                'data' => $this->date 
-            ]));
-        }
-
-        if ($this->isIfaceCeisa) {
-            Redis::publish('portalv2', json_encode([
-                'app' => 'it_inv_checker',
-                'message' => 'date : '.$this->date . ' sync data from ceisa 4.0',
-                'type' => 'info',
-                'data' => $this->date 
-            ]));
-
-            $dataUnsync = viewCeisaRespon::where('TGL_DAFTAR', $this->date)
-            ->orderBy('TGL_DAFTAR', 'DESC')
-            ->get();
-    
-            foreach ($dataUnsync as $key => $valueData) {
-                SyncITInventoryByBCNo::dispatch($valueData->NOMOR_DAFTAR, $valueData->TGL_DAFTAR)->onQueue('SyncITInventoryByBCNo');
+        try {
+            if ($this->isIfaceMega) {
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'it_inv_checker',
+                    'message' => $this->date. ' start mega sync to it inventory now...',
+                    'type' => 'info',
+                    'status' => 'start_mega_resync',
+                    'data' => $this->date 
+                ]));
+        
+                DB::connection('sqlsrv_itinv')->update("SET NOCOUNT ON;EXEC IF_CR_ALL_BYDAY @IFDT_Str='".$this->date."', @SUMFLG=1");
+        
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'it_inv_checker',
+                    'message' => $this->date. ' data sync !! please check on IT Inventory',
+                    'type' => 'green',
+                    'status' => 'success_mega_resync',
+                    'data' => $this->date 
+                ]));
+            } else {
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'it_inv_checker',
+                    'message' => 'Mega sync skipping date : '.$this->date,
+                    'type' => 'orange',
+                    'data' => $this->date 
+                ]));
             }
-        } else {
+    
+            if ($this->isIfaceCeisa) {
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'it_inv_checker',
+                    'message' => 'date : '.$this->date . ' sync data from ceisa 4.0',
+                    'type' => 'info',
+                    'data' => $this->date 
+                ]));
+    
+                $dataUnsync = viewCeisaRespon::where('TGL_DAFTAR', $this->date)
+                ->orderBy('TGL_DAFTAR', 'DESC')
+                ->get();
+        
+                foreach ($dataUnsync as $key => $valueData) {
+                    SyncITInventoryByBCNo::dispatch($valueData->NOMOR_DAFTAR, $valueData->TGL_DAFTAR)->onQueue('SyncITInventoryByBCNo');
+                }
+            } else {
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'it_inv_checker',
+                    'message' => 'date : '.$this->date . ' sync data from ceisa 4.0 is skipped',
+                    'type' => 'orange',
+                    'data' => $this->date 
+                ]));
+            }
+        } catch (\Throwable $th) {
             Redis::publish('portalv2', json_encode([
                 'app' => 'it_inv_checker',
-                'message' => 'date : '.$this->date . ' sync data from ceisa 4.0 is skipped',
-                'type' => 'orange',
-                'data' => $this->date 
+                'message' => 'date '.$this->date.' sync failed server : '.$th->getMessage(),
+                'type' => 'red',
+                'status' => 'failed',
             ]));
         }
     }
