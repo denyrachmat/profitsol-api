3fac61c1d7e6ec9267feb9a5e13b2a10120f34bc
denyrachmat22 <denyrachmat22@gmail.com>
2024-02-09 13:23:27 +0700

    Fix: Model not Array

--- app/Imports/STXI/BIM/ImportCircularTen.php
+++ app/Imports/STXI/BIM/ImportCircularTen.php
@@ -93,7 +93,7 @@ class ImportCircularTen implements ToModel
                         if (!empty($valueModel) && strlen($valueModel) > 4 && count($getDataItem) > 0) {
 
                             $this->data['ten'] = $this->tenNo;
-                            $this->data['model'][] = count($getDataItem) > 0 ? $getDataItem['model'] : [];
+                            $this->data['model'][] = $getDataItem['model'];
                             $this->data['supp_cd'] = count($getDataItem) > 0 ? $getDataItem['listSub'] : '';
                             $this->data['valmodel'][] = count($getDataItem) > 0 ? $getDataItem['valmodel'] : [];
                             $this->data['cekItem'] = $getDataItem;
