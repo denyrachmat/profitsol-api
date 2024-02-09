2e121f7ff6d77e3eb108656c9b4dd8aa8c988ec2
denyrachmat22 <denyrachmat22@gmail.com>
2024-02-09 17:37:35 +0700

    Fix: Coba storage

--- app/Jobs/STXI/BIM/SyncCirTentoOldDMS.php
+++ app/Jobs/STXI/BIM/SyncCirTentoOldDMS.php
@@ -66,7 +66,8 @@ class SyncCirTentoOldDMS implements ShouldQueue
         $pdf = $this->generateDocument($emailDate, true);
         $storepdf = Storage::disk('local')->put('/public/circular_ten/' . $ten . '/' . $ten . '.pdf', $pdf);
         $target_url = 'http://192.168.100.32:8081/stx_api/public/api/'; // Write your URL here
-        $pathFile = '../storage/app/public/circular_ten/' . $ten . '/' . $ten . '.pdf';
+        // $pathFile = '../storage/app/public/circular_ten/' . $ten . '/' . $ten . '.pdf';
+        $pathFile = Storage::url('circular_ten/' . $ten . '/' . $ten . '.pdf');
 
         $client = new Client([
             // Base URI is used with relative requests
