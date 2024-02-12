5f477ffe1086839b90720c57ff0d4434c0771f5b
denyrachmat22 <denyrachmat22@gmail.com>
2024-02-12 17:13:09 +0700

    Fix: Check konten

--- app/Imports/STXI/BIM/ImportCircularTen.php
+++ app/Imports/STXI/BIM/ImportCircularTen.php
@@ -131,6 +131,7 @@ class ImportCircularTen implements ToModel
                 if (count($hasilCekKosong) > 0) {
                     $this->contentArray[] = $row;
                 } else {
+                    $this->data['cekContentJuga'] = $this->contentArray;
                     foreach ($this->contentArray as $keyRow => $valueRow) {
                         $this->tableBuild .= "<tr>";
     
