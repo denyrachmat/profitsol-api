21a359b8d2a0d304990d4f32ddc56d5cef11e04b
denyrachmat22 <denyrachmat22@gmail.com>
2024-03-26 13:20:08 +0700

    Fix: Export ypo daily conf ems2

--- app/Exports/STXI/EMS2/ExportYPODailyConf.php
+++ app/Exports/STXI/EMS2/ExportYPODailyConf.php
@@ -4,34 +4,234 @@ namespace App\Exports\STXI\EMS2;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Carbon\CarbonPeriod;
-class ExportYPODailyConf implements FromCollection
+use Maatwebsite\Excel\Concerns\WithEvents;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class ExportYPODailyConf implements FromCollection, WithHeadings, WithEvents
 {
+    use RegistersEventListeners, Exportable;
     public $fdate, $ldate;
-    function __construct($fdate, $ldate){
+    function __construct($fdate, $ldate)
+    {
         $this->fdate = $fdate;
         $this->ldate = $ldate;
     }
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
-    {
-        $period = CarbonPeriod::create($this->fdate, $this->ldate);
-        $hasil = [];
 
-        foreach ($period as $key => $valuePeriod) {
-            foreach ($this->getData($valuePeriod->format('Y-m-d')) as $key => $valData) {
-                # code...
-            }
-        }
+    function getArrPeriod(): CarbonPeriod
+    {
+        $period = CarbonPeriod::create($this->fdate, $this->ldate)->filter('isWeekday');
+
+        return $period;
     }
 
-    function getData($date){
-        $data = DB::connection('sqlsrv_mega_exim')->table('Z_STXI_YPO_DAILY_CONF')
-        ->where('KSHP_BSGRP', 'SME3IIZMRI')
-        ->where('KSHP_SHPDT', $date)
-        ->get();
+    public function headings(): array
+    {
+        $headDateSect = [];
+        $headDateSectDet = [];
+        foreach ($this->getArrPeriod() as $key => $valuePeriod) {
+            if ($key > 0) {
+                $headDateSect = array_merge($headDateSect, [
+                    'Time Line Supplier (Target ETA YEID)',
+                    'ETD Supplier (am/pm)',
+                    $valuePeriod->format('d/M/Y'),
+                    '',
+                    ''
+                ]);
+
+                $headDateSectDet = array_merge($headDateSectDet, [
+                    '',
+                    '',
+                    'DELIVERY QTY',
+                    'Stock at Sumitronics (Pcs)',
+                    'Balance Stock (pcs)',
+                ]);
+            } else {
+                $headDateSect = [
+                    $valuePeriod->format('d/M/Y'),
+                    '',
+                    ''
+                ];
+
+                $headDateSectDet = array_merge($headDateSectDet, [
+                    'DELIVERY QTY',
+                    'Stock at Sumitronics (Pcs)',
+                    'Balance Stock (pcs)',
+                ]);
+            }
+        }
+
+        // logger($headDateSect);
+
+        return [
+            [
+                'Confirmation Daily PO & Stock F/G N+1  : ' . date('d M Y', strtotime($this->fdate)) . ')',
+            ],
+            [
+                'Nama Supplier : (PT SUMITRONICS INDONESIA)',
+            ],
+            array_merge(
+                [
+                    'No',
+                    'Part No',
+                    'Part Name'
+                ],
+                $headDateSect
+            ),
+            array_merge(
+                [
+                    '',
+                    '',
+                    ''
+                ],
+                $headDateSectDet
+            )
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        $hasil = [];
+
+        foreach ($this->getArrPeriod() as $key => $valuePeriod) {
+            if ($key === 0) {
+                foreach ($this->getData($valuePeriod->format('Y-m-d')) as $keyData => $valData) {
+                    $hasil[] = [
+                        'no' => $keyData + 1,
+                        'part' => $valData->KSHP_ITMCD,
+                        'name' => $valData->MITM_SPTNO,
+                        'dlv'. $key => $valData->KSHP_DELQT,
+                        'stock'. $key => $valData->OPN_QT,
+                        'tot'. $key => $valData->OPN_QT - $valData->KSHP_DELQT,
+                        '1' => '11.00  - 13.00  AM',
+                        '2' => 'AM'
+                    ];
+                }
+            } else {
+                foreach ($hasil as $keyFirst => $valueFirst) {
+                    $cekItem = $this->getData($valuePeriod->format('Y-m-d'), $valueFirst['part']);
+
+                    logger($cekItem);
+
+                    if (count($cekItem) > 0) {
+                        foreach ($this->getData($valuePeriod->format('Y-m-d'), $valueFirst['part']) as $keyData => $valData) {
+                            $hasil[$keyFirst] = array_merge(
+                                $hasil[$keyFirst],
+                                [
+                                    'dlv'. $key => $valData->KSHP_DELQT,
+                                    'stock'. $key => $valData->OPN_QT,
+                                    'tot'. $key => $valData->OPN_QT - $valData->KSHP_DELQT
+                                ]
+                            );
+                        }
+                    } else {
+                        $hasil[$keyFirst] = array_merge(
+                            $hasil[$keyFirst],
+                            [
+                                'dlv'. $key => '0',
+                                'stock'. $key => '0',
+                                'tot'. $key => '0'
+                            ]
+                        );
+                    }
+                }
+
+            }
+        }
+
+        return collect($hasil);
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $highestRow = $event->sheet->getHighestRow();
+                $highestColumn = $event->sheet->getHighestColumn();
+
+                $event->sheet->getDelegate()->getPageSetup()
+                    ->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE)
+                    ->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
+
+                // Style Header
+                $event->sheet->getStyle('A1:A2')->applyFromArray([
+                    'font' => [
+                        'size' => '14',
+                        'bold' => true,
+                        'italic' => true,
+                        'underline' => true
+                    ]
+                ]);
+
+                $event->sheet->getStyle('A3:'.$highestColumn.'4')->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                    ]
+                ]);
+
+                // Buat Table
+                $event->sheet->styleCells(
+                    'A3:'.$highestColumn.$highestRow,
+                    [
+                        'borders' => [
+                            'allBorders' => [
+                                'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            ],
+                        ]
+                    ]
+                );
+
+                // Merge first 3 cols
+                $event->sheet->getDelegate()->mergeCells('A3:A4');
+                $event->sheet->getDelegate()->mergeCells('B3:B4');
+                $event->sheet->getDelegate()->mergeCells('C3:C4');
+
+                $event->sheet->getStyle('A3:'.$highestColumn.'4')->getAlignment()->setHorizontal('center');
+                $event->sheet->getStyle('A3:'.$highestColumn.'4')->getAlignment()->setVertical('center');
+
+                // Merge Date
+                $startCol = 3;
+                foreach ($this->getArrPeriod() as $key => $valuePeriod) {
+                    $event->sheet->getDelegate()->mergeCells($this->toAlpha($startCol).'3:'.$this->toAlpha($startCol + 2).'3');
+                    $event->sheet->getStyle($this->toAlpha($startCol))->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_00);
+
+                    if ($key !== count($this->getArrPeriod()) - 1) {
+                        $event->sheet->getDelegate()->mergeCells($this->toAlpha($startCol + 3).'3:'.$this->toAlpha($startCol + 3).'4');
+                        $event->sheet->getDelegate()->mergeCells($this->toAlpha($startCol + 4).'3:'.$this->toAlpha($startCol + 4).'4');
+                        
+                        $event->sheet->getStyle($this->toAlpha($startCol + 3).'5:'.$this->toAlpha($startCol + 4).$highestRow)->getAlignment()->setHorizontal('center');
+                    }
+                    $startCol = $startCol + 5;
+                }
+            }
+        ];
+    }
+
+    public function toAlpha($num)
+    {
+        for ($r = ""; $num >= 0; $num = intval($num / 26) - 1)
+            $r = chr($num % 26 + 0x41) . $r;
+        return $r;
+    }
+
+    function getData($date, $item = '')
+    {
+        $dataPrep = DB::connection('sqlsrv_mega_exim')->table('Z_STXI_YPO_DAILY_CONF')
+            ->where('KSHP_BSGRP', 'SME3IIZMRI')
+            ->where('KSHP_SHPDT', $date);
+
+        if (!empty ($item)) {
+            $data = $dataPrep->where('KSHP_ITMCD', $item)->get();
+        } else {
+            $data = $dataPrep->get();
+        }
 
         return $data;
     }
--- app/Http/Controllers/STXI/EMS2/YPODailyConfController.php
+++ app/Http/Controllers/STXI/EMS2/YPODailyConfController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use Excel;
+use App\Exports\STXI\EMS2\ExportYPODailyConf;
+
 class YPODailyConfController extends Controller
 {
     public function index() {
@@ -18,6 +21,8 @@ class YPODailyConfController extends Controller
     }
 
     public function store(Request $request) {
+        Excel::store(new ExportYPODailyConf($request->fdate, $request->ldate), 'export_ypo_daily_conf.xlsx', 'public');
         
+        return 'storage/app/public/export_ypo_daily_conf.xlsx';
     }
 }
