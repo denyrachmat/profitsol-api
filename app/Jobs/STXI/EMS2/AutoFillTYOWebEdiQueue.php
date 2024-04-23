1f9a460bc39a2fa36c81f793f6f6e0a170b71f1e
denyrachmat22 <denyrachmat22@gmail.com>
2024-04-23 16:43:43 +0700

    Fix: add error notif

--- app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
+++ app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
@@ -34,37 +34,46 @@ class AutoFillTYOWebEdiQueue implements ShouldQueue
      */
     public function handle(): void
     {
-        Redis::publish('portalv2', json_encode([
-            'app' => 'auto_fill_tyo_webedi',
-            'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : Input to TYO WebEDI on progress !',
-            'type' => 'green',
-            'status' => 'start',
-            'data' => $this->data
-        ]));
-
-        $process = Process::path('D:\app\stx-i-automation\robot-tyo-barcode-creator')
-            ->run('C:\Python311\python.exe -m robocorp.tasks run tasks.py -- --data "' . $this->url . '"');
-
-        TYOA_BC_MSTR::updateorcreate([
-            'TYOAM_PONO' => $this->data[1],
-            'TYOAM_DLVDT' => $this->deliveryDate,
-        ], [
-            'TYOA_ID' => $this->ids,
-            'TYOAM_PONO' => $this->data[1],
-            'TYOAM_ITMCD' => $this->data[0],
-            'TYOAM_QTY' => $this->data[2],
-            'TYOAM_JOBNO' => $this->data[6],
-            'TYOAM_DLVDT' => $this->deliveryDate,
-            'TYOAM_STAT' => $process->successful(),
-            'TYOAM_REMARKS' => $process->errorOutput()
-        ]);
-
-        Redis::publish('portalv2', json_encode([
-            'app' => 'auto_fill_tyo_webedi',
-            'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : '. $process->successful() ? 'Successfully inputed to WEBEdi': 'Failed input to WEBEdi',
-            'type' => $process->successful() ? 'green' : 'red',
-            'status' => 'end',
-            'data' => $this->data
-        ]));
+        try {
+            Redis::publish('portalv2', json_encode([
+                'app' => 'auto_fill_tyo_webedi',
+                'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : Input to TYO WebEDI on progress !',
+                'type' => 'green',
+                'status' => 'start',
+                'data' => $this->data
+            ]));
+    
+            $process = Process::path('D:\app\stx-i-automation\robot-tyo-barcode-creator')
+                ->run('C:\Python311\python.exe -m robocorp.tasks run tasks.py -- --data "' . $this->url . '"');
+    
+            TYOA_BC_MSTR::updateorcreate([
+                'TYOAM_PONO' => $this->data[1],
+                'TYOAM_DLVDT' => $this->deliveryDate,
+            ], [
+                'TYOA_ID' => $this->ids,
+                'TYOAM_PONO' => $this->data[1],
+                'TYOAM_ITMCD' => $this->data[0],
+                'TYOAM_QTY' => $this->data[2],
+                'TYOAM_JOBNO' => $this->data[6],
+                'TYOAM_DLVDT' => $this->deliveryDate,
+                'TYOAM_STAT' => $process->successful(),
+                'TYOAM_REMARKS' => $process->errorOutput()
+            ]);
+    
+            Redis::publish('portalv2', json_encode([
+                'app' => 'auto_fill_tyo_webedi',
+                'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : '. $process->successful() ? 'Successfully inputed to WEBEdi': 'Failed input to WEBEdi',
+                'type' => $process->successful() ? 'green' : 'red',
+                'status' => 'end',
+                'data' => $this->data
+            ]));
+        } catch (\Throwable $th) {
+            Redis::publish('portalv2', json_encode([
+                'app' => 'cirten',
+                'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : Failed to execute.',
+                'type' => 'red',
+                'status' => 'failed',
+            ]));
+        }
     }
 }
