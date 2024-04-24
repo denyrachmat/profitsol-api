89be9a1dd0ce6080680a1ba403d4be3dd9b75b27
denyrachmat22 <denyrachmat22@gmail.com>
2024-04-24 14:43:44 +0700

    Fix: Coba pisahin sesuai sukses / ga

--- app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
+++ app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
@@ -61,28 +61,40 @@ class AutoFillTYOWebEdiQueue implements ShouldQueue
     
             $process = Process::path('D:\app\stx-i-automation\robot-tyo-barcode-creator')
                 ->run('C:\Python311\python.exe -m robocorp.tasks run tasks.py -- --data "' . $this->url . '"');
-    
-            TYOA_BC_MSTR::updateorcreate([
-                'TYOAM_PONO' => $this->data[1],
-                'TYOAM_DLVDT' => $this->deliveryDate,
-            ], [
-                'TYOA_ID' => $this->ids,
-                'TYOAM_PONO' => $this->data[1],
-                'TYOAM_ITMCD' => $this->data[0],
-                'TYOAM_QTY' => $this->data[2],
-                'TYOAM_JOBNO' => $this->data[6],
-                'TYOAM_DLVDT' => $this->deliveryDate,
-                'TYOAM_STAT' => $process->successful(),
-                'TYOAM_REMARKS' => iconv('','UTF-8',$process->errorOutput())
-            ]);
-    
-            Redis::publish('portalv2', json_encode([
-                'app' => 'auto_fill_tyo_webedi',
-                'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : '. $process->successful() ? 'Successfully inputed to WEBEdi': 'Failed input to WEBEdi',
-                'type' => $process->successful() ? 'green' : 'red',
-                'status' => 'end',
-                'data' => $this->data
-            ]));
+            
+            if($process->successful()) {
+                TYOA_BC_MSTR::updateorcreate([
+                    'TYOAM_PONO' => $this->data[1],
+                    'TYOAM_DLVDT' => $this->deliveryDate,
+                ], [
+                    'TYOAM_STAT' => $process->successful(),
+                    'TYOAM_REMARKS' => iconv('','UTF-8',$process->errorOutput())
+                ]);
+        
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'auto_fill_tyo_webedi',
+                    'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : Successfully inputed to WEBEdi',
+                    'type' => 'green',
+                    'status' => 'end',
+                    'data' => $this->data
+                ]));
+            } else {
+                TYOA_BC_MSTR::updateorcreate([
+                    'TYOAM_PONO' => $this->data[1],
+                    'TYOAM_DLVDT' => $this->deliveryDate,
+                ], [
+                    'TYOAM_STAT' => $process->successful(),
+                    'TYOAM_REMARKS' => iconv('','UTF-8',$process->errorOutput())
+                ]);
+        
+                Redis::publish('portalv2', json_encode([
+                    'app' => 'auto_fill_tyo_webedi',
+                    'message' => 'ID ' . $this->ids . ', PO ('.$this->data[1].') : Failed input to WEBEdi',
+                    'type' => 'red',
+                    'status' => 'end',
+                    'data' => $this->data
+                ]));
+            }
         } catch (\Throwable $th) {
             TYOA_BC_MSTR::updateorcreate([
                 'TYOAM_PONO' => $this->data[1],
