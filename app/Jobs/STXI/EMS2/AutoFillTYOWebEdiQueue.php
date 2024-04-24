d9c03382629e5ad1c94abd66663765b35761d066
denyrachmat22 <denyrachmat22@gmail.com>
2024-04-24 14:24:56 +0700

    Fix: Remove comma displacer

--- app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
+++ app/Jobs/STXI/EMS2/AutoFillTYOWebEdiQueue.php
@@ -73,7 +73,7 @@ class AutoFillTYOWebEdiQueue implements ShouldQueue
                 'TYOAM_JOBNO' => $this->data[6],
                 'TYOAM_DLVDT' => $this->deliveryDate,
                 'TYOAM_STAT' => $process->successful(),
-                'TYOAM_REMARKS' => str_replace(',', '.', iconv('','UTF-8',$process->errorOutput()))
+                'TYOAM_REMARKS' => iconv('','UTF-8',$process->errorOutput())
             ]);
     
             Redis::publish('portalv2', json_encode([
