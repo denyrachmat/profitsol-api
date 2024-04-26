94df0a58f0d6f2994ccf8679281c534307d9a99e
denyrachmat22 <denyrachmat22@gmail.com>
2024-04-26 14:07:45 +0700

    Fix: Salah position spq

--- app/Imports/STXI/EMS2/ImportTYOAutoBCCreator.php
+++ app/Imports/STXI/EMS2/ImportTYOAutoBCCreator.php
@@ -42,7 +42,7 @@ class ImportTYOAutoBCCreator implements ToModel, WithStartRow
                 'po_no' => $row[1],
                 'date' => $DLVDT,
                 'qty' => $row[2],
-                'spq' => $row[2],
+                'spq' => $row[3],
                 'job_no' => $row[6],
             ]]));
 
