a3e535ba79fa977a37652983e5c72881e619d1f1
denyrachmat22 <denyrachmat22@gmail.com>
2024-05-06 12:15:02 +0700

    Fix: Add sync delete data

--- app/Http/Controllers/STXI/PC/autoSyncBOMtoPSIController.php
+++ app/Http/Controllers/STXI/PC/autoSyncBOMtoPSIController.php
@@ -25,12 +25,29 @@ class autoSyncBOMtoPSIController extends Controller
         foreach ($getDataPA100 as $key => $value) {
             syncBOMToPSIQueue::dispatch($value, $runTime)->onQueue('syncPA100BOMToPSI');
         }
-
-        $getPSIDataForDelete = BOMSTX_TBL::where('APPROVED', 0)->get();
-        foreach ($getPSIDataForDelete as $keyPSI => $valuePSI) {
-            syncDeletePSItoBOMQueue::dispatch($getDataPA100, $valuePSI, $runTime)->onQueue('deletePA100BOMToPSI');
-        }
         
         return 'Sync BOM Queued, Data to be updated : '.count($getDataPA100);
+    }
+
+    public function syncDeleteData($dataPA100 = []) {
+        $runTime = date('Y-m-d H:i:s');
+        $getPSIDataForDelete = BOMSTX_TBL::where('APPROVED', 0)->get();
+
+        if (count($dataPA100) === 0) {
+            $dataSTXI = DB::connection('sqlsrv_mega_sme')
+            ->select('SET NOCOUNT ON;exec Z_STXI_DOWNLOAD_PA100_BOM_FOR_SYNC_PSI');
+
+            $dataSTXI = array_map(function ($valueDe2) {
+                return (array) $valueDe2;
+            }, $dataSTXI);
+        } else {
+            $dataSTXI = $dataPA100;
+        }
+
+        foreach ($getPSIDataForDelete as $keyPSI => $valuePSI) {
+            syncDeletePSItoBOMQueue::dispatch($dataSTXI, $valuePSI, $runTime)->onQueue('deletePA100BOMToPSI');
+        }
+
+        return 'Sync BOM For delete Queued, Data to be updated : '.count($getPSIDataForDelete);
     }
 }
