48d7870defbe41f29a19974cce2077a5336cc33f
denyrachmat22 <denyrachmat22@gmail.com>
2024-05-08 08:51:32 +0700

    Fix: Coba lagi lah

--- app/Http/Controllers/STXI/EMS2/TYOAutoBarcodeController.php
+++ app/Http/Controllers/STXI/EMS2/TYOAutoBarcodeController.php
@@ -74,7 +74,7 @@ class TYOAutoBarcodeController extends BaseController
         $zip = new \ZipArchive();
         $zip->open($zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
-        $path = Storage::disk('public')->path('upload_tyo_auto_bc_gen/DownloadTYO');
+        $path = Storage::disk('public')->path('upload_tyo_auto_bc_gen/DownloadTYO/'.$id);
         $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
         foreach ($files as $name => $file) {
             // We're skipping all subfolders
