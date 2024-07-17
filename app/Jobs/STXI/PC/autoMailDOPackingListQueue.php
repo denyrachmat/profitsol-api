ad71fadb1421cc1327603e1a880eabb274ef7898
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-17 14:42:19 +0700

    Fix: Update lagi coba

--- app/Jobs/STXI/PC/autoMailDOPackingListQueue.php
+++ app/Jobs/STXI/PC/autoMailDOPackingListQueue.php
@@ -2,13 +2,13 @@
 
 namespace App\Jobs\STXI\PC;
 
+use App\Mail\STXI\PC\DOPackingList;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\STXI\PC\DOPackingList;
 
 class autoMailDOPackingListQueue implements ShouldQueue
 {
