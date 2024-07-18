2059fedd877044c2bfe0aa8a0b664a69cb426666
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-18 08:12:04 +0700

    Fix: Update subject

--- app/Mail/STXI/PC/DOPackingList.php
+++ app/Mail/STXI/PC/DOPackingList.php
@@ -28,7 +28,7 @@ class DOPackingList extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: "DO Picking Confirmation - {$this->data[0]['KSHP_BSGRP']}",
+            subject: "DO Picking Confirmation - {$this->data[0]['KSHP_BSGRP']} ({$this->data[0]['KSHP_DELCD']})",
         );
     }
 
