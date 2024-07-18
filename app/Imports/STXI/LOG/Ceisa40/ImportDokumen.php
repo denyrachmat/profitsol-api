9ba3257ff29b72b01c563f9fe961c701cff69c5f
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-18 11:45:19 +0700

    Fix: Coba pake foreach

--- app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
@@ -180,8 +180,12 @@ class ImportDokumen implements ToModel, WithHeadingRow, SkipsEmptyRows
                             ->get();
 
                         if (count($cekOutgoing) > 0) {
+                            $itemList = [];
+                            foreach ($cekOutgoing as $key => $valueItm) {
+                                $itemList[] = $valueItm['ITMCD'];
+                            }
                             (clone $baseDoc)
-                                ->whereIn('ITMCD', (clone $cekOutgoing)->pluck('ITMCD'))
+                                ->whereIn('ITMCD', $itemList)
                                 ->update([
                                     'BC23BCTYPE' => $row['kode_dokumen'] == 33 ? 'BC3.3' : 'BC1.6',
                                     'BC33DOCNO' => $row['kode_dokumen'] == 33 ? $row['nomor_dokumen'] : NULL,
