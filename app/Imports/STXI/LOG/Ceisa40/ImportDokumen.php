c68d60af12f52939f5d7d235b5dea61fcb5cf5cb
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-18 13:41:30 +0700

    Fix: add logger

--- app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
@@ -187,13 +187,15 @@ class ImportDokumen implements ToModel, WithHeadingRow, SkipsEmptyRows
 
                             logger(json_encode([$itemList, $cekOutgoing]));
 
-                            (clone $baseDoc)
+                            $updated = (clone $baseDoc)
                                 ->whereIn('ITMCD', $itemList)
                                 ->update([
                                     'BC23BCTYPE' => $row['kode_dokumen'] == 33 ? 'BC3.3' : 'BC1.6',
                                     'BC33DOCNO' => $row['kode_dokumen'] == 33 ? $row['nomor_dokumen'] : NULL,
                                     'BC33DOCDT' => $row['kode_dokumen'] == 33 ? $row['tanggal_dokumen'] : NULL,
                                 ]);
+
+                            logger("is updated {$updated}");
                         }
                     }
                 }
