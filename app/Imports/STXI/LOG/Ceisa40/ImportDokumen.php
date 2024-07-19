f239d09105cc221fc85d723e6ace8447d311b5c8
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-19 08:41:42 +0700

    Fix: Typo

--- app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
@@ -182,7 +182,7 @@ class ImportDokumen implements ToModel, WithHeadingRow, SkipsEmptyRows
                         if (count($cekOutgoing) > 0) {
                             logger("cek lagi 22 : {(clone $cekOutgoing)->pluck('ITMCD')} - {$row['kode_dokumen']}");
                             foreach ((clone $baseDoc)
-                            ->whereIn('ITMCD', (clone $cekOutgoing)->pluck('ITMCD'))->get as $key => $value) {
+                            ->whereIn('ITMCD', (clone $cekOutgoing)->pluck('ITMCD'))->get() as $key => $value) {
                                 logger(json_encode($value));
                             }
 
