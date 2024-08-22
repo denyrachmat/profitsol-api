bf43152ea03b8c612b52c312a70252af8f789dd7
denyrachmat22 <denyrachmat22@gmail.com>
2024-08-22 15:04:44 +0700

    Fix: Update tikom

--- app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportDokumen.php
@@ -112,6 +112,21 @@ class ImportDokumen implements ToModel, WithHeadingRow, SkipsEmptyRows
                                         'INVNO' => $row['nomor_dokumen']
                                     ]);
                             }
+                        } else {
+                            $cekLatest = (clone $baseDoc)->first();
+
+                            if (!empty($cekLatest)) {
+                                foreach ($cekOutgoing->pluck('ITMCD') as $key => $valueItm) {
+                                    $explodeData = explode($cekLatest, ";");
+                                    $explodeData[] = $row['nomor_dokumen'];
+
+                                    (clone $baseDocUpdate)
+                                        ->where('ITMCD', $valueItm)
+                                        ->update([
+                                            'INVNO' => implode(";", $explodeData)
+                                        ]);
+                                }
+                            };
                         }
                     }
                 }
