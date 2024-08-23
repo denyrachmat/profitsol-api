a48033b073bd4ef7f026f38b84926b356a97ffcc
denyrachmat22 <denyrachmat22@gmail.com>
2024-08-23 11:43:26 +0700

    Fix: Balikin lagi

--- app/Imports/STXI/LOG/Ceisa40/ImportBarang.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportBarang.php
@@ -39,7 +39,8 @@ class ImportBarang implements ToModel, WithHeadingRow, SkipsEmptyRows
                         ->where('BCTYPE', $cekTempData['TYPE_BC'])
                         ->where('BCDOCDT', $cekTempData['TGL_DAFTAR'])
                         ->where('ITMCD', trim($row['kode_barang']))
-                        ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
+                        ->where('TTLQTY', trim($row['jumlah_satuan']))
+                        // ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
                         ->delete();
 
                     $cekIncoming = ITINVIncoming::where('BCDOCNO', 'LIKE', $noDaftar . '%')
@@ -201,7 +202,8 @@ class ImportBarang implements ToModel, WithHeadingRow, SkipsEmptyRows
                         ->where('BCTYPE', $cekTempData['TYPE_BC'])
                         ->where('BCDOCDT', $cekTempData['TGL_DAFTAR'])
                         ->where('ITMCD', trim($row['kode_barang']))
-                        ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
+                        ->where('TTLQTY', trim($row['jumlah_satuan']))
+                        // ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
                         ->delete();
 
                     $cekOutgoing = ITINVOutgoing::NoLock()->where('BCDOCNO', 'LIKE', $noDaftar . '%')
@@ -216,7 +218,8 @@ class ImportBarang implements ToModel, WithHeadingRow, SkipsEmptyRows
                             ->where('BCTYPE', $cekTempData['TYPE_BC'])
                             ->where('BCDOCDT', $cekTempData["TGL_DAFTAR"])
                             ->where('ITMCD', trim($row['kode_barang']))
-                            ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
+                            ->where('TTLQTY', trim($row['jumlah_satuan']))
+                            // ->havingRaw('SUM(TTLQTY) = ' . trim($row['jumlah_satuan']))
                             ->update([
                                 'PRICE' => $row['cif'] == 0
                                     ? round((int) $row['harga_penyerahan'] / (int) $row['jumlah_satuan'], 4)
