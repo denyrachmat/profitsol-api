9b89cf675ffdfb8f5f986ac0ae13f3aab090c8a1
denyrachmat22 <denyrachmat22@gmail.com>
2024-09-24 13:14:04 +0700

    Fix: Coba lagi

--- resources/views/AMS/AMSEmailTemplate.blade.php
+++ resources/views/AMS/AMSEmailTemplate.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
   # Introduction
-
+  {!!$data->content!!}
   @component('mail::button', ['url' => $approve])
   Approve
   @endcomponent
@@ -8,6 +8,8 @@
   @component('mail::button', ['url' => $reject])
   Reject
   @endcomponent
+
+  or using this URL to view detail : {{env('FE_URL')}}/approvalAction/{{$data->token}}
 
   Thanks,<br>
   {{ config('app.name') }}
