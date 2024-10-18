bb099b500059b387202dafcdb6885d68673128b9
denyrachmat22 <denyrachmat22@gmail.com>
2024-10-18 16:35:02 +0700

    Fix: Update add check item mega

--- app/Imports/STXI/LOG/Ceisa40/ImportBarang.php
+++ app/Imports/STXI/LOG/Ceisa40/ImportBarang.php
@@ -196,8 +196,8 @@ class ImportBarang implements ToModel, WithHeadingRow, SkipsEmptyRows
                     }
 
                     $cekBCStatus = viewCeisaRespon::where('NOMOR_DAFTAR', $noDaftar)->where('TGL_DAFTAR', $cekTempData['TGL_DAFTAR'])->first();
-
-                    if ($cekBCStatus->STAT_MEGABCDOC == 1) {
+                    $cekItemMega = DB::connection('sqlsrv_itinv')->table('VIEW_MITM_TBL')->where('MITM_ITMCD', $row['kode_barang'])->first();
+                    if ($cekBCStatus->STAT_MEGABCDOC == 1 && !empty($cekItemMega)) {
                         $cekHeaderMega = DB::connection('sqlsrv_mega_db')
                             ->table('Z_STXI_VW_CBCDOC')
                             ->where('CBCDOC_BCDOCNO', $noDaftar)
