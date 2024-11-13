1e4906097be5b7a889c3c4f7eb143bac960f592e
denyrachmat22 <denyrachmat22@gmail.com>
2024-11-13 16:16:49 +0700

    Fix: COba update logger

--- app/Traits/AMS/ApprovalActionTraits.php
+++ app/Traits/AMS/ApprovalActionTraits.php
@@ -70,6 +70,7 @@ trait ApprovalActionTraits
                     return $this->handleError("you hasn't provide some data keys on request!!", $checkFil);
                 }
 
+                logger($checkJSON);
                 if ($request->has('msgkey') && !empty($request->msgkey)) {
                     $keyRequest = $checkJSON[$request->msgkey];
                     $cekHist = ApprovalHistDetail::where('amsm_id', $request->amsm_id)->where('amshd_paramstore', 'like', "'%" . $keyRequest . "%'")->first();
