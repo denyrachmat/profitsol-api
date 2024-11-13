e58e4ad78e002ddaf89c39bdd9f23dd48b7db3fa
denyrachmat22 <denyrachmat22@gmail.com>
2024-11-13 13:53:49 +0700

    Fix: Update lagi

--- app/Traits/AMS/ApprovalActionTraits.php
+++ app/Traits/AMS/ApprovalActionTraits.php
@@ -165,7 +165,7 @@ trait ApprovalActionTraits
             // If First or now order more than last order
             if ((int) $valueDet['amsmd_order'] > $getfirstOrder || empty($checkFirst)) {
                 // If Next Order
-                if ($valueDet['amsmd_order'] == (int) $checkLatestOrder + 1) {
+                if ($valueDet['amsmd_order'] == (int) $checkLatestOrder + 1 || empty($checkFirst)) {
 
                     // Sent Notif
                     $hist = ApprovalHistDetail::create([
