6e77c72461705ea55647a4c05d767c1f8046b224
denyrachmat22 <denyrachmat22@gmail.com>
2024-11-21 11:24:25 +0700

    Fix: Update lagi

--- app/Traits/AMS/ApprovalActionTraits.php
+++ app/Traits/AMS/ApprovalActionTraits.php
@@ -184,7 +184,7 @@ trait ApprovalActionTraits
                 }
             } else {
                 // If last order
-                if (!isset($dataMaster->det[$checkLatestOrder])) {
+                if (!isset($dataMaster->det[$checkLatestOrder]) && $valueDet['p_u_username'] === $request->username) {
                     $this->sendingApproval($request, $dataMaster, $checkFirst, $checkLatest, $valueDet, $histToken, $useToken, $nextStat, true);
                     // Delete used token
                     ApprovalTokenDetail::where('id', $useTokenCreate->id)->delete();
