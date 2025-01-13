91c30072a9948f3aa90aefeaa29ee0c9531760d3
denyrachmat22 <denyrachmat22@gmail.com>
2025-01-13 15:49:39 +0700

    Fix: update lagi

--- app/Imports/STXI/BIM/ImportCircularTen.php
+++ app/Imports/STXI/BIM/ImportCircularTen.php
@@ -253,7 +253,7 @@ class ImportCircularTen implements ToModel
                 $hasilSupp = [];
                 foreach ($cekModel->pluck('CIM_ITMCD') as $key => $value) {
                     $cekSupp = $this->getItemMaster($value, '');
-                    if (count($datanya) > 0 && count($cekSupp) > 0) {
+                    if (count($cekSupp) > 0) {
                         $datanya = array_merge($hasilSupp, array_values($cekSupp['listSub']));
                         logger(json_encode($cekSupp));
                         $hasilData = $datanya[0];
