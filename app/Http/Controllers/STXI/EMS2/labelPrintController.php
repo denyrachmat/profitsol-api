02c9749fa768e47506484a1eae329c00048181c8
denyrachmat22 <denyrachmat22@gmail.com>
2025-01-16 14:02:25 +0700

    Fix: Add memory limit

--- app/Http/Controllers/STXI/EMS2/labelPrintController.php
+++ app/Http/Controllers/STXI/EMS2/labelPrintController.php
@@ -199,6 +199,7 @@ class labelPrintController extends BaseController
 
     public function splitData(Request $request)
     {
+        ini_set('memory_limit', '2048M');
         $dataDetail = $request->data['det'] ?? [];
         usort($dataDetail, function ($a, $b) {
             return $a['SPQ_QTY'] <=> $b['SPQ_QTY'];
