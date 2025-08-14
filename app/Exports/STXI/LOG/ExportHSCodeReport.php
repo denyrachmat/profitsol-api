1d1a95b230522d3e339a439ac8a6179e7f50bded
denyrachmat22 <denyrachmat22@gmail.com>
2025-08-14 11:25:16 +0700

    fix: Normalize string comparison for 'Consistent' in registerEvents method of ExportHSCodeReport

--- app/Exports/STXI/LOG/ExportHSCodeReport.php
+++ app/Exports/STXI/LOG/ExportHSCodeReport.php
@@ -410,7 +410,7 @@ class ExportHSCodeReport implements FromCollection, WithHeadings, WithEvents
                 for ($row = 4; $row <= $highestRow; $row++) {
                     $cellValue = $event->sheet->getCell("AU{$row}")->getValue();
                     logger($cellValue);
-                    if (strtoupper(trim($cellValue)) !== 'Consistent') {
+                    if (strtoupper(trim($cellValue)) !== 'CONSISTENT') {
                         $event->sheet->getStyle("A{$row}:{$highestColumn}{$row}")->applyFromArray([
                             'fill' => [
                                 'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
