8e912cbff7903babb027b5d9fdde8936d31a789c
denyrachmat22 <denyrachmat22@gmail.com>
2025-10-24 09:33:52 +0700

    feat: Update CORS configuration to enable allowed origins and headers for improved API access

--- config/cors.php
+++ config/cors.php
@@ -19,21 +19,23 @@ return [
 
     // 'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    // 'allowed_headers' => ['*'], // this is the line i needed to update to solve the issue.
+    'allowed_headers' => ['*'], // this is the line i needed to update to solve the issue.
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => [
-        'https://portal.sumitronics-indonesia.com',
-        'https://intranet.sumitronics-indonesia.com',
-        'https://api.sumitronics-indonesia.com',
-        'http://localhost',
-        'http://127.0.0.1:9000',
-    ],
-    'allowed_headers' => ['X-Requested-With', 'Content-Type', 'X-XSRF-TOKEN', 'Authorization'],
+    // 'allowed_origins' => [
+    //     'https://portal.sumitronics-indonesia.com',
+    //     'https://intranet.sumitronics-indonesia.com',
+    //     'https://api.sumitronics-indonesia.com',
+    //     'http://localhost',
+    //     'http://127.0.0.1:9000',
+    //     'http://localhost:8080',
+    //     'http://stx-api.test',
+    // ],
+    // 'allowed_headers' => ['X-Requested-With', 'Content-Type', 'X-XSRF-TOKEN', 'Authorization'],
 
     'exposed_headers' => [],
 
