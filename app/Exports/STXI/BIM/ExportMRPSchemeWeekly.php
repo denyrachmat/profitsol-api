b91ef7d42d18cfc3d76174f1f310de36f12c1747
denyrachmat22 <denyrachmat22@gmail.com>
2026-01-20 16:54:50 +0700

    fix: Correct lastDate calculation in headings method to use po_rel_date

--- app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
+++ app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
@@ -34,7 +34,7 @@ class ExportMRPSchemeWeekly implements FromCollection, WithHeadings, WithEvents
         $firstDate = $this->data['first_date'];
         $weekCount = $this->data['week_count'] ?? 98;
 
-        $lastDate = (new \DateTime($firstDate))->modify('+' . ($weekCount * 7 - 1) . ' days')->format('Y-m-d');
+        $lastDate = (new \DateTime(date('Y-m-d', strtotime($this->data['po_rel_date']))))->modify('+' . ($weekCount * 7 - 1) . ' days')->format('Y-m-d');
         $lt = $this->data['lt'] ?? 21;
         $firstDayOfMonth = (new \DateTime($firstDate))->modify('first day of this month')->format('Y-m-d');
         $dataHeaders = $this->getData($firstDayOfMonth, $lastDate, $lt);
