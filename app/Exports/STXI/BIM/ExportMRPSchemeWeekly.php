3c915edb1a2bee8b9cec18c563e0c1789bad6633
denyrachmat22 <denyrachmat22@gmail.com>
2026-04-16 16:44:14 +0700

    fix: Adjust date calculation in collection method for correct date formatting

--- app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
+++ app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
@@ -169,7 +169,7 @@ class ExportMRPSchemeWeekly implements FromCollection, WithHeadings, WithEvents
             // For change first row data using original week list
             foreach ($this->dataPerListOriginal[$key] as $keyOri => $valueOri) {
                 if (isset($value[$keyOri])) {
-                    $hasil[$startKeysRows][0] = date('d/m/Y', strtotime($valueOri));
+                    $hasil[$startKeysRows][0] = date('d/m/Y', strtotime($valueOri . ' +1 day'));
                 }
             }
 
