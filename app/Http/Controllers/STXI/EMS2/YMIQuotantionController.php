aab774c9d2c17c01ae7596fa073a76ec74657e5d
denyrachmat22 <denyrachmat22@gmail.com>
2026-04-16 11:42:20 +0700

    fix: Update comparison operator in getData method for correct filter handling

--- app/Http/Controllers/STXI/EMS2/YMIQuotantionController.php
+++ app/Http/Controllers/STXI/EMS2/YMIQuotantionController.php
@@ -130,7 +130,7 @@ class YMIQuotantionController extends BaseController
         if ($req->has('filter')) {
             foreach ($req->filter as $key => $value) {
                 if (!empty($value['value'])) {
-                    if ($value['param'] === 'range') {
+                    if ($value['param'] == 'range') {
                         $data->whereBetween($value['cols'], $value['value']);
                     } else {
                         $data->where($value['cols'], $value['param'], $value['param'] == 'like' ? "%{$value['value']}%": $value['value']);
