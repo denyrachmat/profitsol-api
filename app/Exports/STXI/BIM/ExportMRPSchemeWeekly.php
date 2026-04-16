37384660546f1644244e34b9534b7b61dda50584
denyrachmat22 <denyrachmat22@gmail.com>
2026-04-16 16:10:03 +0700

    fix: Comment out logging statements in headings method for cleaner output

--- app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
+++ app/Exports/STXI/BIM/ExportMRPSchemeWeekly.php
@@ -43,7 +43,7 @@ class ExportMRPSchemeWeekly implements FromCollection, WithHeadings, WithEvents
         // $getLeadTimeList = DB::connection('sqlsrv_mega_sme')->table('MITM_TBL')->select('MITM_ETALT')->distinct()->where('MITM_ETALT', '>', 0)->get()->toArray();
         $getLeadTimeList = $this->getListLT();
 
-        logger("LeadTimeList", $getLeadTimeList);
+        // logger("LeadTimeList", $getLeadTimeList);
         // logger("dataHeaders", $dataHeaders);
 
         $listDataPerLTMega = [];
@@ -56,11 +56,11 @@ class ExportMRPSchemeWeekly implements FromCollection, WithHeadings, WithEvents
 
             if (!empty($getDateData)) {
                 $listDataPerLTMega[(int) $valueLT['lt_(days)']] = $this->getData($firstDayOfMonth, $lastDatePerLT);
-                $this->dataPerListOriginal[(int) $valueLT['lt_(days)']] = $this->getData($this->data['first_date'], $lastDatePerLTOriginal);
+                $this->dataPerListOriginal[(int) $valueLT['lt_(days)']] = $this->getData($firstDayOfMonth, $lastDatePerLTOriginal);
             }
         }
-        logger("listDataPerLTMega", $this->listLTByWeeks);
-        logger("dataPerListOriginal", $this->dataPerListOriginal);
+        // logger("listDataPerLTMega", $this->listLTByWeeks);
+        // logger("dataPerListOriginal", $this->dataPerListOriginal);
 
         $this->dataPerlist = $listDataPerLTMega;
 
