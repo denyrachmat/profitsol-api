bea3f7ae3c1e7789e87080ea5507c0bdfc0241bf
denyrachmat22 <denyrachmat22@gmail.com>
2026-07-13 15:32:40 +0700

    feat: add quiz template import functionality

---
+++ app/Imports/TOS/ImportQuizTemplate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Imports\TOS;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class ImportQuizTemplate implements ToCollection
+{
+    /**
+    * @param Collection $collection
+    */
+    public function collection(Collection $collection)
+    {
+        //
+    }
+}
