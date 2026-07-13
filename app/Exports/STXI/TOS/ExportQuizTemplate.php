c4742ed89058ce383fc1c1bd7dea7e5d963db110
denyrachmat22 <denyrachmat22@gmail.com>
2026-07-13 15:19:25 +0700

    feat: add quiz template export for STXI TOS

---
+++ app/Exports/STXI/TOS/ExportQuizTemplate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exports\STXI\TOS;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class ExportQuizTemplate implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+
+    }
+}
