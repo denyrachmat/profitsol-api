e5ff9df659e835b17d32d9f825c1050bf88ccc7a
denyrachmat22 <denyrachmat22@gmail.com>
2026-07-24 14:08:56 +0700

    feat: Add completion status endpoint to FormController

--- routes/api.php
+++ routes/api.php
@@ -199,6 +199,7 @@ Route::group(['prefix' => 'cms'], function () {
     Route::post('storeAnswers', [FormController::class, 'storeAnswers']);
     Route::post('storeBulkAnswers', [FormController::class, 'storeBulkAnswers']);
     Route::post('cloneForm', [FormController::class, 'cloneForm']);
+    Route::get('completionStatus/{id}', [FormController::class, 'getCompletionStatus']);
     Route::delete('deleteAnswers/{id}/{batchID}', [FormController::class, 'destroyAnswers']);
     Route::get('getConnectedMRS/{id}', [FormController::class, 'getConnectedMRS']);
     Route::get('viewByLinkForm/{link}', [FormController::class, 'viewByLinkForm']);
