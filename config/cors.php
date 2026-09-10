60c75e874dcf3136c51ce01624e48752a9cb61db
denyrachmat22 <denyrachmat22@gmail.com>
2026-09-10 13:41:42 +0700

    fix: Update allowed_origins in CORS configuration to specify trusted domains

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     // 'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://intranet.sumitronics-indonesia.com',
+        'https://portal.sumitronics-indonesia.com',
+        'http://localhost:8080',
+    ],
 
     'allowed_origins_patterns' => [],
 
